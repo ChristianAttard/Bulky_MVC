45befe47984db14547ad319fca2ac34f1e364b0b
Christian Attard <chris.att.91@gmail.com>
2024-08-13 22:20:29 +0200

    server and client side validations

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -4,30 +4,40 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
-    public class CategoryController : Controller
-    {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        public IActionResult Index()
-        {
-            var objCategoryList = _db.Categories.ToList();
-            return View(objCategoryList);
-        }
-
-        public IActionResult Create()
-        {
-            return View();
-        }
-        [HttpPost]
-		public IActionResult Create(Category obj)
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
 		{
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-			return RedirectToAction("Index");
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			var objCategoryList = _db.Categories.ToList();
+			return View(objCategoryList);
 		}
 
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+			}
+			if (obj.Name != null && obj.Name.ToLower() == "test")
+			{
+				ModelState.AddModelError("", "Test is an invalid value");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+			}
+			return View();
+		}
 	}
 }
