2dfbbfcc3909d3313dc428865877d350eae6460b
Christian Attard <chris.att.91@gmail.com>
2024-09-09 23:43:08 +0200

    Concluding ajax javascripting

--- BulkyWeb/wwwroot/js/product.js
+++ BulkyWeb/wwwroot/js/product.js
@@ -1,9 +1,11 @@
-﻿$(document).ready(function () {
+﻿var dataTable;
+
+$(document).ready(function () {
     loadDataTable();
 });
 
 function loadDataTable() {
-    $('#tblData').DataTable({
+    dataTable = $('#tblData').DataTable({
         "ajax": {
             "url": '/admin/product/getall',
             "type": 'GET',
@@ -16,16 +18,42 @@ function loadDataTable() {
             { "data": "author", "width": "15%" },
             { "data": "category.name", "width": "10%" }, // Accessing nested property 'category.name'
             {
-                "data": 'id',                
+                "data": 'id',
                 "render": function (data) {
                     return `
                         <div class="w-75 btn-group" role="group">
                             <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit </a>
-                            <a class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i> Delete </a>
-                        </div>`
+                            <a onClick="Delete('/admin/product/delete/${data}')" class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i> Delete </a>
+                        </div>`;
                 },
                 "width": "25%"
             }
         ]
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload(); // Refresh the data table
+                    toastr.success(data.message); // Show success message
+                },
+                error: function (xhr, status, error) {
+                    toastr.error('An error occurred: ' + xhr.responseText); // Show error message
+                }
+            });
+        }
     });
 }
