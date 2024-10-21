6072272f5fbbaedd045f7416aaca87cf6190275d
Christian Attard <chris.att.91@gmail.com>
2024-10-21 22:16:37 +0200

    Adding foreign key to users

---
+++ Bulky.DataAccess/Migrations/20241021194507_addCompanyToUser.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCompanyToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "City", "Name", "StreetAddress" },
+                values: new object[] { "Tech City", "Tech Solution", "123 Tech Str" });
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "City", "Name", "StreetAddress" },
+                values: new object[] { "Har", "Motherland", "123 test" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Name", "PhoneNumber", "PostalCode", "State", "StreetAddress" },
+                values: new object[] { 4, "Tech City", "British Steel", "99491169", "1234", "Mgarr", "123 Tech Str" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CompanyId",
+                table: "AspNetUsers",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Companies_CompanyId",
+                table: "AspNetUsers",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Companies_CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "City", "Name", "StreetAddress" },
+                values: new object[] { "Har", "Motherland", "123 test" });
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "City", "Name", "StreetAddress" },
+                values: new object[] { "Tech City", "British Steel", "123 Tech Str" });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "City", "Name", "PhoneNumber", "PostalCode", "State", "StreetAddress" },
+                values: new object[] { 1, "Tech City", "Tech Solution", "99491169", "1234", "Mgarr", "123 Tech Str" });
+        }
+    }
+}
